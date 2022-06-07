612f3de525b00fdfc1645675be7c0d4a84aa73dd
Михаил Панафидин <m.panafidin@gmail.com>
2022-06-07 17:33:21 +0300

    3 commit

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
-#include "std.h"
+#include "sdt.h"
 
 int main()
 {
     int a, b;
     printf("Input a and b:\t");
     scanf("%d%d", &a, &b);
+    printf("Sum = %d\n", a + b);
+
+    return 0;
 }
